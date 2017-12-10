a640a19b13be754999b2b25e81a55e522c9dfba5
apaul24 <31377215+apaul24@users.noreply.github.com>
2017-12-10 13:49:09 -0500

    Update smartCar.sh

--- smartCar.sh
+++ smartCar.sh
@@ -2,7 +2,7 @@
 
 #Inititalize bluetooth connection to OBD scanner
 sudo modprobe rfcomm
-sudo rfcomm bind rfcomm0 00:1D:A5:1B:66:74
+sudo rfcomm bind rfcomm0 00:1D:A5:1B:66:74  #MAC address of OBD scanner
 
 #Run Smart Car system
 python smartCar.py
